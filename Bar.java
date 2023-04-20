788e7a4c15e9372026dcea230efd4d693e4619b0
barsedaka <bar.sedaka@gmail.com>
2023-04-20 20:27:35 +0300

    third

--- Bar.java
+++ Bar.java
@@ -1,7 +1,7 @@
 public class Bar{
 	public static void main(String[] args) {
 		for (int i=1;i<=5; i++) {
-			System.out.println("Bar ...");
+			System.out.println("My name is Bar Sedaka");
 		}
 	}
 }
